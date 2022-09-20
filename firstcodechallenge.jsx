b5f48b188b716b5c8db98d1c10578dba371daccd
Anita771 <111849949+Anita771@users.noreply.github.com>
2022-09-20 13:52:12 +0300

    Update firstcodechallenge.jsx

--- firstcodechallenge.jsx
+++ firstcodechallenge.jsx
@@ -1,1 +1,13 @@
+function test (phoneNumber) {
+  if (phoneNumber[0]=== '+')
+    {return 'known';
+    }
+    else {
+        return 'unknown';
+        }
+         }
+   console.log ((test('+77228'); // print known
+   // console.log ((test('-77228'); print unknown
+   // console.log ((test('77228'); print unknown
+   
 
